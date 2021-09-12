fb25e1df86df74ced8866b772fd576301faf0440
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2021-09-11 21:27:09 -0400

    Add UI

---
+++ Calculator/src/main/java/com/example/calculator/fxController.java
@@ -0,0 +1,15 @@
+package com.example.calculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class fxController {
+    // Temp examples
+   // @FXML
+   // private Label welcomeText;
+
+    //@FXML
+    //protected void onHelloButtonClick() {
+       // welcomeText.setText("Welcome to JavaFX Application!");
+    //}
+}
