89c6629451bf5d94357a8bc76753e56a4fe15fb1
Ethan Bowers <ejbowers@bsu.edu>
2021-09-12 12:07:44 -0400

    Added Calculation functions and tests

--- Calculator/src/main/java/com/example/calculator/Calculator.java
+++ Calculator/src/main/java/com/example/calculator/Calculator.java
@@ -1,5 +1,6 @@
 package com.example.calculator;
 
+
 public class Calculator {
     public static String ConversionToQuaternary(String input) {
         int sBase = 10;
@@ -13,5 +14,21 @@ public class Calculator {
         int dBase  = 10;
         return Integer.toString(
                 Integer.parseInt(input, sBase), dBase);
+    }
+
+    public static String addition(String firstNum, String secondNum){
+        return Integer.toString(Integer.parseInt(firstNum) + Integer.parseInt(secondNum));
+    }
+
+    public static String subtraction(String firstNum, String secondNum){
+        return Integer.toString(Integer.parseInt(firstNum) - Integer.parseInt(secondNum));
+    }
+
+    public static String multiplication(String firstNum, String secondNum){
+        return Integer.toString(Integer.parseInt(firstNum) * Integer.parseInt(secondNum));
+    }
+
+    public static String division(String firstNum, String secondNum){
+        return Integer.toString(Integer.parseInt(firstNum) / Integer.parseInt(secondNum));
     }
 }
--- Calculator/src/main/java/com/example/calculator/CalculatorTest.java
+++ Calculator/src/main/java/com/example/calculator/CalculatorTest.java
@@ -18,5 +18,33 @@ public class CalculatorTest {
         String actual = Calculator.ConversionToStandard("1021");
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void additionTest(){
+        String expected = "31";
+        String actual = Calculator.addition("15", "16");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void subtractionTest(){
+        String expected = "22";
+        String actual = Calculator.subtraction("30", "8");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void multiplicationTest(){
+        String expected = "18";
+        String actual = Calculator.multiplication("6", "3");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void divisionTest(){
+        String expected = "2";
+        String actual = Calculator.division("16", "8");
+        assertEquals(expected, actual);
+    }
 }
 
